e8b017b0d407c13e887b6606cbfc83b78b94a019
Tarek Aziz <mdaziz3289@gmail.com>
2023-06-07 17:57:29 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ac-sgyxco9-shard-00-00.shgh1ow.mongodb.net:27017,ac-sgyxco9-shard-00-01.shgh1ow.mongodb.net:27017,ac-sgyxco9-shard-00-02.shgh1ow.mongodb.net:27017/?ssl=true&replicaSet=atlas-b9z6n7-shard-0&authSource=admin&retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get("/", (req, res) => {
+  res.send("Summer camp server is running");
+});
+
+app.listen(port, () => {
+  console.log(`Summer camp server is running on port ${port}`);
+});
