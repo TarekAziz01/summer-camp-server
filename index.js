cb9d0d02848e40347fcee200e6e1b770b1f61fa7
Tarek Aziz <mdaziz3289@gmail.com>
2023-06-09 16:25:31 +0600

    making instructor api added

--- index.js
+++ index.js
@@ -50,12 +50,22 @@ async function run() {
       res.send(result);
     })
 
-    app.patch("user/admin/:id", async (req, res) => {
-      const id = req.params.id;
+    app.patch("/user/admin/:id", async (req, res) => {
       const filter = { _id: new ObjectId(id) };
       const updateDoc = {
         $set: {
           role: "admin",
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    app.patch("/user/instructor/:id", async (req, res) => {
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "instructor",
         },
       };
       const result = await usersCollection.updateOne(filter, updateDoc);
