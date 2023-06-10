3bdef6337d611faacda10044b68ec05e1481be15
Tarek Aziz <mdaziz3289@gmail.com>
2023-06-10 12:30:30 +0600

    jwt post api added

--- index.js
+++ index.js
@@ -32,6 +32,16 @@ async function run() {
     const classCollection = client.db("summerDb").collection("classes");
     const cartCollection = client.db("summerDb").collection("carts");
 
+
+
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      })
+      res.send({token})
+    })
+    
     //users...................
 
     app.get('/users', async (req, res) => {
