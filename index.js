ac5e444fb1ab70a6932a163bba1371397b511b6d
Tarek Aziz <mdaziz3289@gmail.com>
2023-06-11 12:05:54 +0600

    instructor verification system added

--- index.js
+++ index.js
@@ -89,6 +89,18 @@ async function run() {
       res.send(result);
     })
 
+    //instructor checking
+    app.get('/users/instructor/:email', verifyJWT, async (req, res) => {
+      const email = req.params.email;
+      if (req.decoded.email !== email) {
+        res.send({ instructor: false })
+      }
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      const result = { instructor: user?.role === 'instructor' };
+      res.send(result);
+    })
+
     app.patch("/user/admin/:id", async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
