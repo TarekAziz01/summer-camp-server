770349f3044e0607c4a5a1b427ac17480c61940a
Tarek Aziz <mdaziz3289@gmail.com>
2023-06-15 08:59:30 +0600

    payment intent added

--- index.js
+++ index.js
@@ -242,6 +242,20 @@ async function run() {
       res.send(result);
     });
 
+    app.post("/create-payment-intent", async (req, res) => {
+      const { price } = req.body;
+      const amount = price * 100;
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        payment_method_types: ['card']
+      });
+      res.send({
+        clientSecret: paymentIntent.client_secret
+      });
+    })
+
+
 
 
     // Send a ping to confirm a successful connection
